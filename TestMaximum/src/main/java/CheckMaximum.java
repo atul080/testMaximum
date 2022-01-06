7b2139572c232077d0d63f3f5c962916ca2c6216
Atul Srivastav <atul.srivastav.ap@nielsen.com>
2022-01-06 21:33:13 +0530

    Added: Maximum with more than three param

--- TestMaximum/src/main/java/CheckMaximum.java
+++ TestMaximum/src/main/java/CheckMaximum.java
@@ -4,65 +4,34 @@
 * Generics Programs
 * */
 
+import java.util.*;
+
 public class CheckMaximum {
-    /*
-    * findMax method with three INTEGER @param
-    * @return INTEGER
-    * checking maximum among three values.
-    * */
-    public static Integer findMax(Integer a,Integer b,Integer c)
-    {
-        if((a.compareTo(b)>0)&&(a.compareTo(c)>0))
-            return a;
-        else
-        if((b.compareTo(a)>0)&&(b.compareTo(c)>0))
-            return b;
-        else
-            return c;
-    }
-    /*
-     * findMax method with three FLOAT @param
-     * @return FLOAT
-     * checking maximum among three values.
-     * */
-    public static Float findMax(Float a,Float b,Float c)
-    {
-        if((a.compareTo(b)>0)&&(a.compareTo(c)>0))
-            return a;
-        else
-        if((b.compareTo(a)>0)&&(b.compareTo(c)>0))
-            return b;
-        else
-            return c;
-    }
     /*
      * findMax method with three STRING @param
      * @return STRING
-     * checking maximum among three values.
+     * checking maximum among given values.
      * */
-    public static String findMax(String a,String b,String c)
+    public static <E extends Comparable<E>> void findMax(ArrayList a)
     {
-        if((a.compareTo(b)>0)&&(a.compareTo(c)>0))
-            return a;
-        else
-        if((b.compareTo(a)>0)&&(b.compareTo(c)>0))
-            return b;
-        else
-            return c;
+        Collections.sort(a);
+        System.out.println("max is: "+a.get(a.size()-1));
     }
 
 
     public static void main(String[] ar)
     {
-        //calling findMax method with 3 integer values.
-        System.out.println("5,16,9 and max is: "+findMax(5,16,9));
-        //float variable declarations
-        float a=5.6f;
-        float b=16.6f;
-        float c=9.2f;
-        //calling findMax method with float values.
-        System.out.println("5.4,16.6,9.2 and max is: "+findMax(a,b,c));
-        //calling findMax method with string values.
-        System.out.println("Apple,Peach,Banana and max is: "+findMax("Apple","Peach","Banana"));
+        //making object  of scanner class.
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of elements you want to enter.");
+        int n=sc.nextInt();
+        //declaring a arraylist by taking size from user.
+        ArrayList elements=new ArrayList(n);
+        //taking n elements
+        System.out.println("Enter the "+n+" elements.");
+        for(int i=0;i<n;i++)
+            elements.add(sc.next());
+        //calling findMax method to find max
+        findMax(elements);
     }
 }
